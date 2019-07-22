1295f2715cc354dd5081970f5712502e39853126
Chris.Morgan <chris.morgan@clearcloud.co.uk>
2019-07-22 13:45:24 +0000

    Adding DLM policy

---
+++ main.tf
@@ -0,0 +1,36 @@
+#
+# Lifecycle policy
+#
+resource "aws_dlm_lifecycle_policy" "this" {
+  description        = "DLM lifecycle policy"
+  execution_role_arn = "${aws_iam_role.dlm_lifecycle_role.arn}"
+  state              = "${var.state}"
+
+  policy_details {
+    resource_types = ["VOLUME"]  #VOLUME is the only option currently
+
+    schedule {
+      name = "${var.name}"
+
+      create_rule {
+        interval      = "${var.interval}"
+        interval_unit = "HOURS" #HOURS is the only option currently
+        times         = ["${var.times}"]
+      }
+
+      retain_rule {
+        count = "${var.retention}"
+      }
+
+      tags_to_add = {
+        SnapshotCreator = "Data lifecycle manager"
+      }
+
+      copy_tags = "${var.copy_tags}"
+    }
+
+    target_tags = {
+      "${var.target_tags_key}" = "${var.target_tags_value}"
+    }
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "interval" {
+  default = "24"
+}
+variable "retention" {
+  default = "24"
+}
+variable "target_tags_value" {}
+variable "target_tags_key" {}
+variable "state" {
+  default = "ENABLED"
+}
+variable "copy_tags" {
+  default = "true"
+}
+variable "name" {}
+variable "times" {
+  description = "Time to run the backup"
+  default = "02:15"
+}
